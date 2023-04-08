c13f265b3410c4f14e71ff11c5cc3b6810f9019a
edmund_hu <vhudp@163.com>
2023-04-08 09:01:02 +0000

    update mxIndex_samples/build.sh.
    Signed-off-by: edmund_hu <vhudp@163.com>

--- mxIndex_samples/build.sh
+++ mxIndex_samples/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #g++ -std=c++11 -march=armv8-a -fPIC -fstack-protector-all -Wno-sign-compare -D_FORTIFY_SOURCE=2 -O3 -Wall -Wextra -DFINTEGER=int -fopenmp -I../mxIndex/include -I/usr/local/include -I/usr/local/gtest/include -I/usr/local/Ascend/driver/include/dvpp/ -o TestAscendIndexSQ TestAscendIndexSQ.cpp -L../mxIndex/host/lib -L/usr/local/lib -L/usr/local/gtest/lib -L/usr/local/Ascend/driver/lib64 -lopenblas -lfaiss -lascendfaiss -lgtest
-
-rm -rf  ./build
+if [ -d ./build ];then
+ rm -rf  ./build
+fi
 mkdir build
 cd build
 cmake ..
